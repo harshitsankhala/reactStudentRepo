ec219d527ac25694e9185913c70b5eccfb8b199b
Harshit Sankhala <harshitsankhala@gmail.com>
2021-04-15 18:39:02 +0530

    Init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'react-bootstrap';
+import Student from './student';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <Button variant="primary">Primary</Button>{' '} */}
+      <Student />
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +21,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
